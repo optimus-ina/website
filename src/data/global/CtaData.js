4243d5c8b3374cdf2dde26b70d5a7fc92f019771
Maisnam Raju <maisnam.raju@zid.sa>
2023-09-13 19:47:34 +0700

    Updates

--- src/data/global/CtaData.js
+++
@@ -1,9 +0,0 @@
-export const ctaData = {
-  header: "Need a solution that meets your budget and timeline?",
-  subHeader:
-    "We build solutions tailored to your needs and budget. Contact us today and get a free consultation.",
-  button: {
-    title: "contact us",
-    link: "/contact",
-  },
-};
--- src/helpers/Animate.js
+++
@@ -1,3 +0,0 @@
-export const SmoothScroll = () => {
-  document.getElementById("root").scrollIntoView({ behavior: "smooth" });
-};
